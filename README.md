fdcf0381d336f4d46c289f04c5afd7b910cdde85
Yuri Gerassimov <ygerassimov@itl.agency>
2023-01-18 16:20:43 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,136 @@
+# Jurager/Deployer #
+
+This is the simple bash script to implement zero-downtime deployment with some NodeJS frameworks, also can be used to automate deployment of your any other applications.
+
+## Requirements ##
+
+For reading `deploy.config.json` configuration, additional packages is required.
+
+To install Ubuntu/Debian
+
+```sh
+sudo apt-get install jq
+```
+
+To install on Fedora/CentOS
+
+```sh
+sudo dnf install jq
+```
+
+## Installation ##
+
+Download `deployer.sh`, move it in your project directory (see usage), or install globally by moving to `/usr/local/bin`
+
+## Usage ##
+
+In this example we have application written over NuxtJS, serve it using Nginx.
+
+We want to implement green/blue (zero-downtime) deployment on this application.
+
+> We use two applications at once. At the time of deployment, we build the application which is not running. When all deployment task is done, one application is stopped another is starting. At this moment Nginx  switches to another upstream, transparently to user.
+
+First, define the upstream in Nginx with sample configuration
+
+```
+upstream backend {
+    server 127.0.0.1:3000 fail_timeout=0;
+    server 127.0.0.1:3001 fail_timeout=0;
+}
+```
+
+Then, in server directive, change the location as follows
+```
+location / {
+    proxy_pass                          http://backend; # set the address of the Node.js instance here
+    proxy_next_upstream error timeout   invalid_header http_500;
+    proxy_connect_timeout               2;
+    proxy_set_header Host               $host;
+    proxy_set_header X-Real-IP          $remote_addr;
+    proxy_set_header X-Forwarded-For    $proxy_add_x_forwarded_for;
+    proxy_redirect                      off;
+    proxy_buffering off;
+}
+```
+Application is running in daemon mode using supervisord with two configurations as follows
+
+example.blue.conf
+```
+[program:myapp-blue]
+directory=/home/myapp/blue/
+command=/bin/bash -c 'nuxt start'
+user=myapp
+autostart=true
+autorestart=true
+redirect_stderr=true
+stdout_logfile=/var/log/supervisor/myapp-blue.log
+```
+
+example.green.conf
+```
+[program:myapp-green]
+directory=/home/myapp/green/
+command=/bin/bash -c 'nuxt start'
+user=myapp
+autostart=true
+autorestart=true
+redirect_stderr=true
+stdout_logfile=/var/log/supervisor/myapp-green.log
+```
+
+Structure of application will be following:
+```
+/home/
+    /myapp/
+        /green/
+            myapp.js
+        /blue
+            myapp.js
+        deploy.config.json
+``` 
+
+Configuration `deploy.config.json` must be in directory near `green` and `blue`
+
+```json
+{
+  "current":"green",
+  "commands":{
+    "prepare":[
+      "git pull"
+    ],
+    "install":[
+      "npm i"
+    ],
+    "build":[
+      "npm run build"
+    ],
+    "restart":[
+      "supervisorctl start merchant-#NEXT_COLOR#",
+      "supervisorctl stop merchant-#CURRENT_COLOR#"
+    ]
+  },
+  "logging":{
+    "enabled":true,
+    "file":"deploy.log"
+  }
+}
+```
+
+To list all available commands, use
+
+```shell
+deployer.sh -h
+Tool used to implement zero downtime deployment.
+
+Syntax: deployer.sh [-i|v|c|h]
+Options:
+c  Echo current color.
+i  Run commands from 'commands.install' group immediately after prepare
+v  Print version.
+```
+
+To run the deployment process, run following command in directory with configuration, and wait for process to complete
+
+```shell
+cd /home/myapp && deployer.sh
+```
---
+++ deployer.sh
@@ -0,0 +1,317 @@
+#!/bin/bash
+
+#
+# Shell script for green/blue deployment.
+#
+# Notes:
+# - SED is assumed
+# - JQ is assumed
+# - UNIX target environment with SSH is assumed
+#
+
+CONFIG_FILE=$PWD'/deployer.config.json'
+SCRIPT_LOCK=$PWD'/deployer.lock'
+RUN_INSTALL=false
+
+VERSION=1.0
+DESCRIPTION='Tool used to implement zero downtime deployment.'
+
+SCRIPT_NAME=$(basename "$0")
+
+SED=$(which sed)
+JQ=$(which jq)
+
+# Color variables
+#
+GREEN=$'\033[1;32m'
+BLUE=$'\033[1;34m'
+NC=$'\033[0m'
+
+# Check if lockfile exists
+#
+if [[ -e $SCRIPT_LOCK ]]; then
+
+   # Output
+   #
+   echo "$(tput setaf 1)Deploying already in progress.$(tput sgr 0)"
+
+   # Terminate
+   #
+   exit 0
+fi
+
+# Create lockfile
+#
+touch "$SCRIPT_LOCK"
+
+# Listen for script termination
+#
+trap 'rm -f "$SCRIPT_LOCK"; exit $?' INT TERM EXIT
+
+## Error handling and terminating
+##
+error()
+{
+   # Output
+   #
+   echo "$(tput setaf 1)$1$(tput sgr 0)"
+
+   # Terminate
+   # 
+   exit 0
+}
+
+## Show help
+##
+help()
+{
+   # Output
+   #
+   echo "$DESCRIPTION"
+   echo
+   echo "Syntax: $SCRIPT_NAME [-i|v|c|h]"
+   echo "Options:"
+   echo "c  Echo current color."
+   echo "i  Run commands from 'commands.install' group immediately after prepare"
+   echo "v  Print version."
+
+   # Terminate
+   # 
+   exit 1
+}
+
+## Show version
+##
+version()
+{
+  # Output
+  #
+  echo "$DESCRIPTION"
+  echo "Version: $VERSION"
+
+  # Terminate
+  #
+  exit 1
+}
+
+
+# JSON processor needs to be installed
+#
+if ! [ -x "$(command -v jq)" ]; then
+
+   # Output
+   #
+   echo "$(tput setaf 1)supported JSON processor not found.$(tput sgr 0)"
+
+   # Output
+   #
+   echo 'Please download and install from https://stedolan.github.io/jq/'
+
+   # Terminate
+   #
+   exit 1
+fi
+
+# Deployment file not exists
+#
+if ! [ -f "$CONFIG_FILE" ]; then
+   
+   # Error
+   #
+   error "Configuration not exists, please first create $CONFIG_FILE"
+fi
+
+# Get current deployment
+#
+CURRENT_COLOR=$( $JQ -r .current "$CONFIG_FILE" )
+
+## Show current
+##
+current()
+{
+   # Output
+   #
+   echo "Current deployed version: ${!CURRENT_COLOR}$(echo "$CURRENT_COLOR" | tr "[:lower:]" "[:upper:]")$NC"
+
+   # Terminate
+   # 
+   exit 1
+}
+
+
+# Process input arguments
+#
+while getopts ":ivch" option; do
+  case ${option} in
+    i ) 
+         # Use installation flag
+         #
+         RUN_INSTALL=true
+
+         # Output
+         #
+         echo "$(tput setaf 3)Warning! $(tput sgr 0)Installation commands will be executed immediately after \"commands.prepare\""
+      ;;
+    v )  version
+      ;;
+    c )  current
+      ;;
+    h )  help
+      ;;
+    \? ) help
+      ;;
+  esac
+done
+
+# Get next deployment
+#
+NEXT_COLOR=$( [ "$CURRENT_COLOR" == 'blue' ] && echo 'green' || echo 'blue' )
+
+# Current color uppercase
+#
+CC_UPPERCASE=$(echo "$CURRENT_COLOR" | tr '[:lower:]' '[:upper:]')
+
+# Next color uppercase
+#
+NC_UPPERCASE=$(echo "$NEXT_COLOR" | tr '[:lower:]' '[:upper:]')
+
+# Output
+#
+echo "$(tput setaf 3)Direction of deployment is $(tput sgr 0) ${!CC_UPPERCASE}$(echo "$CURRENT_COLOR" | tr "[:lower:]" "[:upper:]")$NC $(tput setaf 3)=>$(tput sgr 0) ${!NC_UPPERCASE}$(echo "$NEXT_COLOR" | tr "[:lower:]" "[:upper:]")$NC"
+
+# Input
+#
+read -p "$(tput setaf 3)Are you sure? Start deployment (y/n)?$(tput sgr 0)" -n 1 -r
+
+# Output
+#
+echo
+
+# Terminate
+#
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+   [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1
+fi
+
+## Check if logging is enabled
+##
+if [ $($JQ -r ".logging.enabled" "$CONFIG_FILE") = true ]
+then
+
+  # Close standard error descriptor
+
+  ## Write output to file
+  ##
+  exec 2> >( while read -r line; do
+
+   # Output
+   #
+   echo "[$(date +"%Y-%m-%d %T")] ${line}";
+
+   done > $( $JQ -r .logging.file "$CONFIG_FILE" ) )
+fi
+
+# ------------------------------------------------------------------
+# For real deployments, this is when you would update your project
+# files in `$PWD/src/deployments/$NEXT_COLOR`, maybe via git pull.
+# ------------------------------------------------------------------
+
+# Output
+#
+echo "$(tput setaf 2)Running commands from \"commands.prepare\" $(tput sgr 0)"
+
+# Change directory
+#
+cd "$NEXT_COLOR" || error "Error, directory \"$NEXT_COLOR\" not found"
+
+
+## Running prepare commands
+##
+for k in $( $JQ -r '.commands.prepare | keys | .[]' "$CONFIG_FILE" ); do
+    
+   # Parse Command
+   #
+   command=$( $JQ -r ".commands.prepare[$k]" "$CONFIG_FILE" );
+
+   # Output
+   #
+   echo "$(tput setaf 3)Running command $command $(tput sgr 0)"
+
+   # Running deploy commands
+   #
+   $command || error "Error, running command \"$command\" from \"commands.prepare\" failed"
+done
+
+
+# If running install commands option received
+#
+if $RUN_INSTALL ; then
+
+   # Output
+   #
+   echo "$(tput setaf 2)Running commands from \"commands.install\" $(tput sgr 0)"
+
+   # Running install commands
+   #
+   for k in $( $JQ -r '.commands.install | keys | .[]' "$CONFIG_FILE"); do
+       
+      # Parse Command
+      #
+      command=$( $JQ -r ".commands.install[$k]" "$CONFIG_FILE" );
+
+      # Output
+      #
+      echo "$(tput setaf 3)Running command $command $(tput sgr 0)"
+
+      # Running deploy commands
+      #
+      $command || error "Error, running command \"$command\" from \"commands.install\" failed"
+   done
+fi
+
+# Output
+#
+echo "$(tput setaf 2)Running commands from \"commands.build\" $(tput sgr 0)"
+
+# Running build commands
+#
+for k in $( $JQ -r '.commands.build | keys | .[]' "$CONFIG_FILE" ); do
+    
+   # Parse Command
+   #
+   command=$( $JQ -r ".commands.build[$k]" "$CONFIG_FILE" );
+
+   # Output
+   #
+   echo "$(tput setaf 3)Running command $command $(tput sgr 0)"
+
+   # Running deploy commands
+   #
+   $command || error "Error, running command \"$command\" from \"commands.install\" failed"
+done
+
+# Update current deployment
+#
+$JQ --arg a "${NEXT_COLOR}" '.current = $a' "$CONFIG_FILE" > "tmp" && mv "tmp" "$CONFIG_FILE"
+
+# Output
+#
+echo "$(tput setaf 2)Running commands from \"commands.restart\" $(tput sgr 0)"
+
+# Running restart deployment commands
+#
+for k in $( $JQ -r '.commands.restart | keys | .[]' "$CONFIG_FILE" ); do
+    
+   # Parse Command
+   #
+   command=$( $JQ -r ".commands.restart[$k]" "$CONFIG_FILE" | $SED -e "s/#NEXT_COLOR#/$NEXT_COLOR/g;s/#CURRENT_COLOR#/$CURRENT_COLOR/g")
+
+   # Output
+   #
+   echo "$(tput setaf 3)Running command $command $(tput sgr 0)"
+
+   # Running deploy commands
+   #
+   $command || error "Error, running command \"$command\" from \"commands.restart\" failed"
+done
